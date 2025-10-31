ce72a10519012108deaeb3be8af76cd6f5e59183
UzForex <atabekovshodlik.1@gmail.com>
2025-10-31 16:17:23 +0000

    pp

--- admin.js
+++ admin.js
@@ -6,8 +6,18 @@ const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.on("message", function (msg) {
   const chatId = msg.chat.id;
+  const firstName = msg.chat.first_name;
+
   console.log(msg);
-  bot.sendMessage(chatId, "xush kelipsiz");
+  bot.sendMessage(chatId, `xush kelipsiz, ${firstName} `, {
+    reply_markup: {
+      keyboard: [
+        [{ text: "boshlash" }],
+        [{ text: "menu" }, { text: "sozlamalar" }],
+      ],
+      remove_keyboard: true 
+    },
+  });
 });
 
 console.log("bot ishga tushdi..");
